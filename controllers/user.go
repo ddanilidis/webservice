32c45832df1f5f029b6fe67c8d5e147e14605b0e
ddanilidis <ddanilidis@gmail.com>
2024-04-02 12:56:58 +0300

    Controller Works

---
+++ controllers/user.go
@@ -0,0 +1,20 @@
+package controller
+
+import (
+	"net/http"
+	"regexp"
+)
+
+type userController struct {
+	userIDPattern *regexp.Regexp
+}
+
+func (uc userController) ServeHTTP(w http.ResponseWriter, r *http.Response) {
+	w.Write([]byte("Hello from controller"))
+}
+
+func newUserController() *userController {
+	return &userController{
+		userIDPattern: regexp.MustCompile(`^/users/(\d+)/?`),
+	}
+}
